46b2aaf3fd978864151136fc91d389ce6a815974
Mona Lisa <info@alphacaptura.com>
2018-09-17 16:53:09 -0600

    git push 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -92,9 +92,9 @@ export default class Contact extends Component {
                             <ul className="actions">
                                 {this.state.show ?
                                     <div>
-                                        <li> <a className="special" href={`mailto:info@alphacaptura.com?subject=Alpha%20Captura%20Free%20Trial%20-%20${this.state.name}&amp;body=Hi!%0A%0AMy%20name%20is%20${this.state.name}%20and%20i%20want%20to%20try%20your%20service%20on%20a%20free%20trial.%0A`}>Send Email</a></li>
                                         <li><input onClick={this.send} disabled={!this.showButton} type="submit" value="Send" className="special" /></li>
                                         <li><input onClick={this.clean} type="reset" value="Clean form" /></li>
+                                        <li> <a className="special" href={`mailto:info@alphacaptura.com?subject=Alpha%20Captura%20Free%20Trial%20-%20${this.state.name}&amp;body=Hi!%0A%0AMy%20name%20is%20${this.state.name}%20and%20i%20want%20to%20try%20your%20service%20on%20a%20free%20trial.%0A`}>Send direct email</a></li>
                                     </div>
                                     :
                                     null
